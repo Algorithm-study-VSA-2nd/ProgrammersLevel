bb3fc0e8831cebcb76b0a46dd399bd05044cadb6
ohmygook <rnrgusgh77@naver.com>
2023-06-25 21:48:27 +0900

    6회차 1번 문제 upload

--- 6회차/Heap_더맵게.py
+++ 6회차/Heap_더맵게.py
@@ -1,27 +1,20 @@
 # def solution(scoville, K):
 # 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
-
-# min-heap 이용해서(by heapify()) 부모 노드 값 꺼내
+# min-heap 이용해서(by heapify()) 부모 노드 값 꺼내서
 import heapq
-scoville=[9,1,10,2,3,12]
-K=7
-cnt=0
-num=len(scoville)
-for j in range(num):
+def solution(scoville, K):
+    cnt = 0
     heapq.heapify(scoville)
-    min1 = heapq.heappop(scoville)
-    min2 = heapq.heappop(scoville)
-    min_num=min1+2*min2
-
-    heapq.heappush(scoville,min_num)
-    cnt+=1
-    if min_num > K:
+    while (1):
+        if len(scoville) <= 1 and scoville[0] < K:
+            return -1
+        if scoville[0] >= K:
             break
-    else: continue
 
-print(cnt)
-    
+        min1 = heapq.heappop(scoville)
+        min2 = heapq.heappop(scoville)
+        min_num = min1+2*min2
 
-
-    
-        
+        heapq.heappush(scoville, min_num)
+        cnt += 1
+    return cnt
