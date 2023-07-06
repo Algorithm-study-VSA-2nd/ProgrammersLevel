e8b43b777e76dc49b7013db2e4872575772a5011
tag_you.s <101778867+tag-you@users.noreply.github.com>
2023-07-06 12:23:59 +0900

    Update 1번_K번째 수.py

--- 7회차/1번_K번째 수.py
+++ 7회차/1번_K번째 수.py
@@ -1,1 +1,16 @@
-
+def solution(array, commands):
+    answer = []
+    # answer에 넣을 빈 int 값 생성
+    in_answer = 0
+    for command in commands:
+        # 조건에 맞는 변수들 생성
+        i = command[0]
+        j = command[1]
+        k = command[2]
+        # index에 맞게 -1 처리
+        new_array = array[i-1:j]
+        # 오름차순 분류
+        new_array.sort()
+        in_answer = new_array[k-1]
+        answer.append(in_answer)
+    return answer
